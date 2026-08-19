a98ba27955f110d920c68b0c58a254dae6a15aac
Fan Yang <128557286+yang200347@users.noreply.github.com>
2026-08-19 13:24:36 +0800

    Correct drop-off analysis date range

--- sql/06_funnel_dropoff_analysis.sql
+++ sql/06_funnel_dropoff_analysis.sql
@@ -13,8 +13,7 @@ WITH relevant_events AS (
   FROM
     `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   WHERE
-    _TABLE_SUFFIX BETWEEN '20201101' AND '20210131'
-    AND event_name IN (
+_TABLE_SUFFIX BETWEEN '20201116' AND '20210131'    AND event_name IN (
       'view_item',
       'add_to_cart',
       'begin_checkout',
