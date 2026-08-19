0a06991a1a8a1e29df659c05dce48d80db50b540
Fan Yang <128557286+yang200347@users.noreply.github.com>
2026-08-19 13:22:51 +0800

    Correct funnel date range after data quality check

--- sql/05_conversion_funnel.sql
+++ sql/05_conversion_funnel.sql
@@ -13,7 +13,8 @@ WITH relevant_events AS (
   FROM
     `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   WHERE
-    _TABLE_SUFFIX BETWEEN '20201101' AND '20210131'
+    -- Exclude dates with incomplete add_to_cart tracking
+    _TABLE_SUFFIX BETWEEN '20201116' AND '20210131'
     AND event_name IN (
       'view_item',
       'add_to_cart',
